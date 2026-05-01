832cd5558bbd48e30af240c5bb66d075ae76fbbe
ishaan-007 <ishaansc2017@gmail.com>
2026-05-01 18:19:25 +0530

    something

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import community_route, trip_routes_2, user_routes, trip_routes
+from app.routes import community_route, user_routes, trip_routes
 
 app = FastAPI(title="WanderWise")
 
@@ -15,8 +15,8 @@ app.add_middleware(
 
 # ✅ Routes
 app.include_router(user_routes.router, prefix="/api", tags=["Users"])
-#app.include_router(trip_routes.router, prefix="/api", tags=["Trips"])
-app.include_router(trip_routes_2.router, prefix="/api", tags=["Trips_2"])
+app.include_router(trip_routes.router, prefix="/api", tags=["Trips"])
+#app.include_router(trip_routes_2.router, prefix="/api", tags=["Trips_2"])
 app.include_router(community_route.router, prefix="/api", tags=["Community"])
 
 @app.get("/")
